e3d7e11d3dfb0979ff67c8156f4c27cc3bb9fa9e
tomByrer <tomByrer@gmail.com>
2014-08-22 06:07:15 -0600

    Update PSON.js

--- dist/PSON.js
+++ dist/PSON.js
@@ -15,7 +15,7 @@
  */
 
 /**
- * @license PSON (c) 2013 Daniel Wirtz <dcode@dcode.io>
+ * @license PSON v 2.0.0 (c) 2013 Daniel Wirtz <dcode@dcode.io>
  * Released under the Apache License, Version 2.0
  * see: https://github.com/dcodeIO/PSON for details
  */
